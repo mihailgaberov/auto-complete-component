cf65fa9318300011c3f86abb6086f99a92aa262e
mihailgaberov <mihail.gaberov@gmail.com>
2024-11-29 13:24:46 +0100

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import styles from './App.module.scss'
+
+function App() {
+  
+
+  return (
+    <div className={styles.container}>
+      zi beginning...
+    </div>
+  )
+}
+
+export default App
