c6aa088c9b4dd18f7c116340f83629d586351af5
mihailgaberov <mihail.gaberov@gmail.com>
2025-02-01 12:22:25 +0100

    test: enhance Input component tests to include focus and focus out handlers

--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -4,14 +4,28 @@ import { vi } from "vitest";
 
 describe("Input Component", () => {
   it("should render without crashing", () => {
-    render(<Input onChangeHandler={() => {}} onKeyDown={() => {}} value="" />);
+    render(
+      <Input
+        onChangeHandler={() => {}}
+        onKeyDown={() => {}}
+        value=""
+        onFocus={() => {}}
+        onFocusOut={() => {}}
+      />
+    );
     expect(screen.getByRole("textbox")).toBeInTheDocument();
   });
 
   it("should call onChangeHandler when input value changes", () => {
     const onChangeHandler = vi.fn();
     render(
-      <Input onChangeHandler={onChangeHandler} onKeyDown={() => {}} value="" />
+      <Input
+        onChangeHandler={onChangeHandler}
+        onKeyDown={() => {}}
+        value=""
+        onFocus={() => {}}
+        onFocusOut={() => {}}
+      />
     );
     const input = screen.getByRole("textbox");
     fireEvent.change(input, { target: { value: "test" } });
@@ -19,12 +33,28 @@ describe("Input Component", () => {
   });
 
   it("should be on focus when rendered", () => {
-    render(<Input onChangeHandler={() => {}} onKeyDown={() => {}} value="" />);
+    render(
+      <Input
+        onChangeHandler={() => {}}
+        onKeyDown={() => {}}
+        value=""
+        onFocus={() => {}}
+        onFocusOut={() => {}}
+      />
+    );
     expect(screen.getByRole("textbox")).toHaveFocus();
   });
 
   it("should have placeholder text", () => {
-    render(<Input onChangeHandler={() => {}} onKeyDown={() => {}} value="" />);
+    render(
+      <Input
+        onChangeHandler={() => {}}
+        onKeyDown={() => {}}
+        value=""
+        onFocus={() => {}}
+        onFocusOut={() => {}}
+      />
+    );
     expect(
       screen.getByPlaceholderText("Type a country name...")
     ).toBeInTheDocument();
